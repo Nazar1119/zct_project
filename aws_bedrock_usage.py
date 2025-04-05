730b4b2d968e31ab24bd50220ef430e99d4c84b6
Oleksandr Petsa <SANYOK7531@users.noreply.github.com>
2025-04-05 21:16:05 +0200

    AWS Bedrock Usage

---
+++ aws_bedrock_usage.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+
+bedrock_runtime = boto3.client("bedrock-runtime", region_name="us-east-2")
+
+prompt = "What the capital of Ukraine?"
+
+kwargs = {
+  "modelId": "arn:aws:bedrock:us-east-2:668610423384:inference-profile/us.amazon.nova-pro-v1:0",
+  "contentType": "application/json",
+  "accept": "application/json",
+  "body": json.dumps({
+    "inferenceConfig": {
+      "max_new_tokens": 1000
+    },
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "text": prompt
+          }
+        ]
+      }
+    ]
+  })
+}
+
+response = bedrock_runtime.invoke_model(**kwargs)
+
+body = json.loads(response['body'].read())
+print(body)
