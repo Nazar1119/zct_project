464b2ce8f9ed1e5da07d38f672f1935d3f75326c
Sputaiok1925 <sputajdaniil59@gmail.com>
2025-04-05 22:44:50 +0200

    exemple

--- bedrock_sputai_version.py
+++ bedrock_sputai_version.py
@@ -1,7 +1,5 @@
 from aws_bedrock_usage import get_response
 
-prompt = "What exercises should I do for my biceps?"  # Замените на любой текст, который хотите отправить модели
-
+prompt = "What exercises should I do for my biceps?"
 response = get_response(prompt)
-
 print(response)
